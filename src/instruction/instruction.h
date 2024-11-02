7bf0cae1780e62faafafb8589acd68c9c9ca45b5
mos <Moosmegens@gmail.com>
2024-11-02 22:20:58 +0100

    bitshift

--- src/instruction/instruction.h
+++ src/instruction/instruction.h
@@ -15,7 +15,8 @@ typedef enum instructionSet{
     MOVEM,          // move memory to reg {register, memoryPos}
     LOAD,           // load number into memory {memoryPos,number}
     IOP,            // instruction for output , {memoryPos, type} (0:char,1:string,2:short,3:int,4:charNumber)
-
+    SHL,            // bitshift left        {register,amount}
+    SHR,            // bitshift left right  {register,amount}
     
     // end of instruction set
     HALT
